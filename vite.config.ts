be388f68328ca52f5159c2773976e1e82970b1cc
Sirlian Cassar <Sirlian.cassarg@gmail.com>
2026-04-15 13:56:06 +0200

    chore: update dependencies and fix styling

--- vite.config.ts
+++ vite.config.ts
@@ -18,11 +18,19 @@ export default defineConfig({
     electron({
       main: {
         entry: 'electron/main.ts',
+        vite: {
+          build: {
+            lib: {
+              entry: 'electron/main.ts',
+              formats: ['cjs'],
+              fileName: () => 'main.cjs',
+            },
+          },
+        },
       },
       preload: {
         input: path.join(__dirname, 'electron/preload.ts'),
       },
-      renderer: process.env.NODE_ENV === 'test' ? undefined : {},
     }),
   ],
 })
